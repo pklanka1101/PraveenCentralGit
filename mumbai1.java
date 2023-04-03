71ccd917add1511ad73f0f06e6bf8c77d1afd036
praveenlanka <praveenlanka@gmail.com>
2023-04-03 17:09:27 +0000

    my second code commit

--- mumbai1.java
+++ mumbai1.java
@@ -1,5 +1,19 @@
-public class HelloWorld {
+import java.util.Scanner;
+
+public class FibonacciSeries {
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
-  }
+	         
+      int n, t1 = 0, t2 = 1;
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter the number of terms: ");
+      n = sc.nextInt();
+      System.out.print("Fibonacci Series: ");
+      for (int i = 1; i <= n; ++i) {
+         System.out.print(t1 + " ");
+         int sum = t1 + t2;
+         t1 = t2;
+         t2 = sum;
+       }
+   }
 }
+
